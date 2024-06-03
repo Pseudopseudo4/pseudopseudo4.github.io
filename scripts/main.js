2ad01a92f91b1fa50329e9b7dc18ecef2aa2fd66
Pseudopseudo4 <88638660+Pseudopseudo4@users.noreply.github.com>
2024-06-03 11:33:01 -0400

    2024-06-03

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let myButton    = document.querySelector("button");
+let myHeading   = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name: ");
+    if (!myName) {
+        myName = "fellow wizard"
+    }
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome, ${myName}!`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome, ${storedName}!`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
